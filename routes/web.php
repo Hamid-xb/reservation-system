a1cdaf5d5665b29d4c6a0617c8c10072cac15fd1
Hamid-xb <120631726+Hamid-xb@users.noreply.github.com>
2026-05-11 16:52:10 +0200

    Add restaurant detail view template with layout and dynamic content

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'show'])->name('restaurants.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
